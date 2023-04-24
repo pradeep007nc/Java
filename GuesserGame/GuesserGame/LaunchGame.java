5cb47d3fa6eef587562899b8919baab6f6f7127a
Pradeep <110987303+pradeep007nc@users.noreply.github.com>
2023-04-24 09:20:16 +0530

    Delete GuesserGame directory

--- GuesserGame/GuesserGame/LaunchGame.java
+++
@@ -1,8 +0,0 @@
-public class LaunchGame {
-    public static void main(String[] args) {
-        umpire umpire = new umpire();
-        umpire.getNumberFromGuesser();
-        umpire.getNumberFromPlayers();
-        umpire.declareResult();
-    }
-}
